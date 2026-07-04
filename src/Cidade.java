c22405c69f45eedf745bbfcbc397171a17d06813
RubensDutra <rubensdutramf@gmail.com>
2026-07-04 15:07:28 -0300

    feat: adicionada classe Cidade

---
+++ src/Cidade.java
@@ -0,0 +1,22 @@
+public class Cidade {
+
+    private String nome;
+
+    public Cidade(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return this.nome;
+    }
+
+}
