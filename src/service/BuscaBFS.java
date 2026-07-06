df875b2f89d1093f96eaea26726cff486d527ad2
RubensDutra <rubensdutramf@gmail.com>
2026-07-06 14:49:52 -0300

    feat: inicializa algoritmo de busca BFS

--- src/service/BuscaBFS.java
+++ src/service/BuscaBFS.java
@@ -14,11 +14,30 @@ public class BuscaBFS {
 
         Queue<Integer> fila = new LinkedList<>();
 
-        boolean [] cidadesVisitadas = new boolean[qntCidades];
+        boolean[] cidadesVisitadas = new boolean[qntCidades];
 
         Map<Integer, Integer> pais = new HashMap<>();
 
-        List<Cidade> caminho =  new ArrayList<>();
+        List<Cidade> caminho = new ArrayList<>();
+
+
+        int indiceOrigem = grafo.buscarIndiceCidade(cidadeOrigem);
+
+        if (indiceOrigem == -1) {
+            return caminho;
+        }
+
+        int indiceDestino = grafo.buscarIndiceCidade(cidadeDestino);
+
+        if (indiceDestino == -1) {
+            return caminho;
+        }
+
+
+        fila.add(indiceOrigem);
+        cidadesVisitadas[indiceOrigem] = true;
+        pais.put(indiceOrigem, -1);
+
 
         return caminho;
     }
