124b1871b86d705c3f97098c808ea68ffb028865
RubensDutra <rubensdutramf@gmail.com>
2026-07-16 15:20:15 -0300

    feat: integra GraphStream ao painel de visualização

--- src/view/PainelVisualizacaoGrafo.java
+++ src/view/PainelVisualizacaoGrafo.java
@@ -1,49 +1,42 @@
 package view;
 
-import model.Grafo;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.ui.swing_viewer.SwingViewer;
 import org.graphstream.ui.swing_viewer.ViewPanel;
 import org.graphstream.ui.view.Viewer;
-import service.BuscaBFS;
-import service.BuscaDFS;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class PainelVisualizacaoGrafo extends JPanel {
 
-    private Graph grafoVisual;
+    private final Graph grafoVisual;
 
-    private Viewer viewer;
+    private final Viewer viewer;
 
-    private ViewPanel viewPanel;
+    private final ViewPanel painelGraphStream;
 
     public PainelVisualizacaoGrafo() {
 
-
         setLayout(new BorderLayout());
 
-        setBorder(BorderFactory.createTitledBorder("Mapa das Cidades"));
-
-        inicializarGraphStream();
-
-    }
-
-    private void inicializarGraphStream() {
+        setBorder(BorderFactory.createTitledBorder("Visualização do Grafo"));
 
         System.setProperty("org.graphstream.ui", "swing");
 
-        grafoVisual = new SingleGraph("Mapa");
+        grafoVisual = new SingleGraph("Mapa das Cidades");
 
-        viewer = new SwingViewer(grafoVisual, Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
+        viewer = new SwingViewer(
+                grafoVisual,
+                Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD
+        );
 
         viewer.enableAutoLayout();
 
-        viewPanel = (ViewPanel) viewer.addDefaultView(false);
+        painelGraphStream = (ViewPanel) viewer.addDefaultView(false);
 
-        add(viewPanel, BorderLayout.CENTER);
+        add(painelGraphStream, BorderLayout.CENTER);
 
     }
 
